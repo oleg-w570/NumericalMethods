418d7a83e65a191adb3e90ff18647e3d2b6cba17
oleg-w570 <ol3g.zor1n@yandex.ru>
2022-12-19 15:40:17 +0300

    бамп

--- NumMethLab2/app/mainwindow.cpp
+++ NumMethLab2/app/mainwindow.cpp
@@ -7,9 +7,14 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     ui->chartview1->setChart(new QChart);
-    ui->chartview1->chart()->legend()->hide();
+    ui->chartview1->chart()->legend()->setAlignment(Qt::AlignBottom);
     ui->chartview2->setChart(new QChart);
-    ui->chartview2->chart()->legend()->hide();
+    ui->chartview2->chart()->legend()->setAlignment(Qt::AlignBottom);
+    ui->chartview1->setRubberBand(QChartView::RectangleRubberBand);
+    ui->chartview2->setRubberBand(QChartView::RectangleRubberBand);
+    ui->chartview1->chart()->setTitle("График решений");
+    ui->chartview2->chart()->setTitle("График погрешности");
+
 //    ui->table->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
 
 }
@@ -39,13 +44,18 @@ void MainWindow::on_pushButtonRun_clicked()
 //    qDebug() << diff << "\n" << num_sol << "\n" << sec_sol << "\n";
 
     QLineSeries *num_solution = new QLineSeries;
+    num_solution->setName("ν(x)");
     QLineSeries *second_solution = new QLineSeries;
+    QString sec_name = mode == TEST ? "u(x)" : "ν₂(x)";
+    second_solution->setName(sec_name);
     QLineSeries *difference = new QLineSeries;
+    difference->setName("|ν(x)-"+sec_name+"|");
+
+
 
     ui->table->setRowCount(N+1);
     ui->table->setColumnCount(4);
-    QString sec_str = mode == TEST ? "u(x)" : "v2(x)";
-    ui->table->setHorizontalHeaderLabels(QStringList() << "x" << "v(x)" << sec_str << "|v(x)-"+sec_str+"|");
+    ui->table->setHorizontalHeaderLabels(QStringList() << "x" << "ν(x)" << sec_name << "|ν(x)-"+sec_name+"|");
 
     double x = 0.0;
     for (unsigned long long i = 0; i < num_sol.size(); i++)
@@ -68,8 +78,8 @@ void MainWindow::on_pushButtonRun_clicked()
     ui->chartview2->chart()->createDefaultAxes();
 
     QString ref = "Для решения задачи использована равномерная сетка с числом разбиений n = " + QString::number(N)
-        + ".\nЗадача должна быть решена с погрешностью не более ε = 0.5e-6.\nЗадача решена с погрешностью ";
-    ref += mode == TEST ? "Ɛ₁ =  " : "ε2 =  ";
+        + ".\nЗадача должна быть решена с погрешностью не более Ɛ = 0.5e-6.\nЗадача решена с погрешностью ";
+    ref += mode == TEST ? "Ɛ₁ =  " : "Ɛ₂ =  ";
     ref += QString::number(*eps) + ".\n";
     ref += mode == TEST ? "Максимальное отклонение аналитического и численного решения наблюдается в точке x = " + QString::number(eps_pos) + "."
         : "Максимальное разность численных решений в общих узлах сетки наблюдается в точке x = " + QString::number(eps_pos) + ".";
